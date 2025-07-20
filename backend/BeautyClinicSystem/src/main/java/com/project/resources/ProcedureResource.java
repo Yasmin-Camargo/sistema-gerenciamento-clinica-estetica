c09c1525568c8ef07474f49fb576d6c40dd27164
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2025-07-20 14:13:34 -0300

    feat: update database configuration and endpoints

--- backend/BeautyClinicSystem/src/main/java/com/project/resources/ProcedureResource.java
+++ backend/BeautyClinicSystem/src/main/java/com/project/resources/ProcedureResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/proceduresName")
+@RequestMapping("/procedures")
 @RequiredArgsConstructor
 public class ProcedureResource {
 
